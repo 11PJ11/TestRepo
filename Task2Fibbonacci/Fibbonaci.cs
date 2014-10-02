d080a2e0182bdeb3e3147608b0d5856e1416a24c
SpikeSrb <spasojevic.ivan@gmail.com>
2014-10-02 23:08:42 +0200

    fibbonaci test

---
+++ Task2Fibbonacci/Fibbonaci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Fibbonacci
+{
+    public class Fibbonaci
+    {
+        public  double  FibonacciTest(int n)
+        {
+            double a = 0;
+           double  b = 1;
+            // In N steps compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                double temp = a;
+                a = b;
+                b = temp + b;
+            }
+             return a;
+        }
+    }
+}
---
+++ Task2Fibbonacci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2Fibbonacci
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            Fibbonaci Fibonacci = new Fibbonaci();
+            
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Fibonacci.FibonacciTest(i));
+            }
+
+        }
+    }
+}
